197ae735e323090c56942ee3539c0e7af6d6e99b
palatej <jean.palate@nbb.be>
2020-10-05 18:39:34 +0200

    Extended results (tau, linearized...)

--- R/jd3_stsoutliers.R
+++ R/jd3_stsoutliers.R
@@ -7,7 +7,6 @@ NULL
 #' @param y 
 #' @param level 
 #' @param slope 
-#' @param cycle 
 #' @param noise 
 #' @param seasonal 
 #' @param X 
@@ -21,7 +20,7 @@ NULL
 #' @export
 #'
 #' @examples
-stsoutliers<-function(y, level=1, slope=1, cycle=-1, noise=1
+stsoutliers<-function(y, level=1, slope=1, noise=1
               , seasonal=c("Trigonometric", "Dummy", "Crude", "HarrisonStevens", "Fixed", "Unused"),
               X=NULL,X.td=NULL, cv=0, tcv=0, estimation.forward=c("Score", "Point", "Full"), estimation.backward=c("Point", "Score", "Full")
               ){
@@ -42,26 +41,31 @@ stsoutliers<-function(y, level=1, slope=1, cycle=-1, noise=1
       
   
   jsts<-.jcall("demetra/sts/r/StsOutliersDetection", "Ldemetra/sts/r/StsOutliersDetection$Results;", "process", ts_r2jd(y), 
-              as.integer(level), as.integer(slope), as.integer(cycle), as.integer(noise), seasonal, matrix_r2jd(X),
+              as.integer(level), as.integer(slope), as.integer(noise), seasonal, matrix_r2jd(X),
               cv, tcv, estimation.forward, estimation.backward)
   model<-list(
     y=as.numeric(y),
     variables=proc_vector(jsts, "variables"),
     X=proc_matrix(jsts, "regressors"),
     b=proc_vector(jsts, "b"),
-    bcov=proc_matrix(jsts, "bvar")
+    bcov=proc_matrix(jsts, "bvar"),
+    components=proc_matrix(jsts, "cmps"),
+    linearized=proc_vector(jsts, "linearized")
   )
   
   l0<-proc_numeric(jsts, "initialbsm.levelvar")
   s0<-proc_numeric(jsts, "initialbsm.slopevar")
   seas0<-proc_numeric(jsts, "initialbsm.seasvar")
   n0<-proc_numeric(jsts, "initialbsm.noisevar")
+  tau0=proc_matrix(jsts, "initialtau")
+  
   
   l1<-proc_numeric(jsts, "finalbsm.levelvar")
   s1<-proc_numeric(jsts, "finalbsm.slopevar")
   seas1<-proc_numeric(jsts, "finalbsm.seasvar")
   n1<-proc_numeric(jsts, "finalbsm.noisevar")
-
+  tau1=proc_matrix(jsts, "finaltau")
+  
   ll0<-proc_likelihood(jsts, "initiallikelihood.")
   ll1<-proc_likelihood(jsts, "finallikelihood.")
   
@@ -72,13 +76,15 @@ stsoutliers<-function(y, level=1, slope=1, cycle=-1, noise=1
         level=l0,
         slope=s0,
         seasonal=seas0,
-        noise=n0
+        noise=n0,
+        tau=tau0
       ),
       final=list(
         level=l1,
         slope=s1,
         seasonal=seas1,
-        noise=n1
+        noise=n1,
+        tau=tau1
       )
     ),
     likelihood=list(initial=ll0, final=ll1)),
